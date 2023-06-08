ac55768d34e9c4d15ca14d441578e09696ff52eb
Rodrigo Wippel <rodrigow1992@gmail.com>
2023-06-08 09:09:04 -0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,12 +8,12 @@ const Home: NextPage = () => {
     <>
       <Head>
         <title>Santé Coding Task</title>
-        <meta name="description" content="Santé Coding Task" />
+        <meta name="description" content="T3 Stack - Lab" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-          <h1 className="text-5xl font-bold">Santé Coding Task</h1>
+          <h1 className="text-5xl font-bold">T3 Stack - Lab</h1>
           <div className="w-2/3">
             <Contacts />
           </div>
